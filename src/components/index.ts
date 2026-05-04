cc7009f4d4967f19bcb2f67e3c38586d3558eaba
Papuna Fshaveli <pmachurishvili@bitnet.ge>
2026-05-04 20:56:07 +0400

    revised translations

--- src/components/index.ts
+++ src/components/index.ts
@@ -20,7 +20,6 @@ export { default as StatisticsCard } from "./statistics-card/StatisticsCard";
 export { AppText } from "./text";
 export { default as DifficultyRing } from "./difficulty-ring/DifficultyRing";
 export { default as ScoreChangeIndicator } from "./score-change-indicator/ScoreChangeIndicator";
-export { default as SignInModal } from "./sign-in/SignInModal";
 export { default as ConfirmNameModal } from "./sign-in/ConfirmNameModal";
 export { default as ForceUpdateModal } from "./sign-in/ForceUpdateModal";
 export { default as SoftUpdateModal } from "./sign-in/SoftUpdateModal";
